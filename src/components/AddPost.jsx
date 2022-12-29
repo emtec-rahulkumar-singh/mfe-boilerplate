b3692a22c80a586959e3de901205e4991c807981
Rahul Singh <rahulkumar.singh@emtecinc.com>
2022-12-29 16:03:58 +0530

    create data added

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,43 +1,61 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const AddPost = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    contact: '',
-  });
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   contact: '',
+  // });
 
-  // const handleChange = (e) => {
-  //   if(e.target.type ==)
-  //   const target = event.target;
-  //   const value = target.type === 'checkbox' ? target.checked : target.value;
-  //   const name = target.name;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
 
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const contactChangeHandler = (event) => {
+    setContact(event.target.value);
+  };
 
   const handleSubmit = (e) => {
-    console.log('in submit');
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formDataObj = Object.fromEntries(formData.entries());
-    console.log(formDataObj);
+    const reqBody = {
+      name,
+      email,
+      contact
+    }
+    console.log(reqBody);
+    axios.post('http://localhost:8080/api/users', reqBody)
+      .then((response) => {
+        console.log('Data Added')
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      setName('');
+      setEmail('');
+      setContact('');
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Name</Form.Label>
-        <Form.Control placeholder="Name" value={name} />
-
+        <Form.Control placeholder="Name" value={name} onChange={nameChangeHandler} required/>
+        
         <Form.Label>Email</Form.Label>
-        <Form.Control placeholder="Email" value={email} />
+        <Form.Control placeholder="Email" value={email} onChange={emailChangeHandler} required/>
 
         <Form.Label>Contact</Form.Label>
-        <Form.Control placeholder="Contact" value={contact} />
+        <Form.Control type="number" placeholder="Contact" value={contact} onChange={contactChangeHandler} required/>
       </Form.Group>
 
       <Button variant="primary" type="submit">
